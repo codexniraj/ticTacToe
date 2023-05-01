90484986acc6a7f8ff7aa2f1c256856cfe0ecd11
Niraj Yogesh Wani <106158322+codexniraj@users.noreply.github.com>
2023-05-01 15:20:06 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
+
+  const handleClick = (position) => {
+    if (board[position] === null && winner === null) {
+      const newBoard = [...board];
+      newBoard[position] = player;
+      setBoard(newBoard);
+      if (player === "X") {
+        setPlayer("O");
+      } else {
+        setPlayer("X");
+      }
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="board">
+        {board.map((value, index) => (
+          <div
+            key={index}
+            className={`cell ${value}`}
+            onClick={() => handleClick(index)}
+          >
+            {value}
+          </div>
+        ))}
+      </div>
+      {winner ? <div className="winner">{winner} wins!</div>
